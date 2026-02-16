069ffd54a7f64024d43f421216d7cf58dea5a992
alvin1052 <alvin10821@gmail.com>
2026-02-16 15:28:48 +0700

    change next config

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,6 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
       },
-      new URL(process.env.NEXT_PUBLIC_API_IMAGE_URL as string),
     ],
   },
 };
